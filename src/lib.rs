42ecb5ab6a20a1465b80c0bfa3310310d0781b27
trevyn <trevyn-git@protonmail.com>
2022-01-30 14:47:30 +0300

    it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+pub fn now() -> String {
+    let minutes = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        / 60;
+    let animal = minutes % (60 * 24); // minute of the day
+    let adjective = (minutes / (60 * 24)) - 19022; // day number
+    format!(
+        "{}-{}",
+        include_str!("adjectives.txt")
+            .split('\n')
+            .nth(adjective as usize)
+            .unwrap(),
+        include_str!("animals.txt")
+            .split('\n')
+            .nth(animal as usize)
+            .unwrap()
+    )
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(super::now().split('-').collect::<Vec<&str>>().len(), 2);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    print!("{}", animal_time::now());
+}
